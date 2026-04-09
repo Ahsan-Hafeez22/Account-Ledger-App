e63a3dc46c58e9db0de995854e18b96b70350898
Ahsan-Hafeez22 <ahsanhafeez0324@gmail.com>
2026-04-10 02:28:15 +0500

    fcm token

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+import 'package:account_ledger/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:account_ledger/app.dart';
 import 'package:account_ledger/core/dependency_injection/service_locator.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initServiceLocator();
-  runApp(const AccountLedger());
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    await initServiceLocator();
+    runApp(const AccountLedger());
+  } catch (e) {
+    debugPrint("Initialization error: $e");
+  }
 }
